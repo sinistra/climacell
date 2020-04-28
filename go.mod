38867d5915ac8161613b518aa7329b4b6ee0442a
Paul Taylor <paul@nms.com.au>
2020-04-28 10:52:11 +1000

    fetch data from Climacell

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sinistra/climacell
+
+go 1.14
+
+require github.com/joho/godotenv v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("could not load env file.")
+	}
+
+	req, err := http.NewRequest(
+		http.MethodGet,
+		"https://api.climacell.co/v3/weather/forecast/hourly?lat=42.3826&lon=-71.1460&fields=temp",
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("error creating HTTP request: %v", err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("apikey", os.Getenv("CLIMACELL_API_KEY"))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("error sending HTTP request: %v", err)
+	}
+	responseBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("error reading HTTP response body: %v", err)
+	}
+
+	log.Println("We got the response:", string(responseBytes))
+}
